0fd2d626ca409b36ed98531f7e3112006604d311
Tony Kerr <46275713+PoeticDeath@users.noreply.github.com>
2026-02-08 23:39:29 -0500

    V1.1.0
    Better power outage safety.

--- RecordHourly.py
+++ RecordHourly.py
@@ -8,7 +8,7 @@ while True:
         dt2 = dt1.replace(minute=0, second=0)
         folder = f"{dt1.year}/{dt1.month}/{dt1.day}/{dt1.hour}/{i}"
         os.makedirs(folder)
-        os.popen(f"ffmpeg -t {h - (dt1 - dt2)} -i 'rtsp://admin:@192.168.1.108:554/trackID={i}' -y -c copy '{folder}/{dt1.minute}.{dt1.second}.mp4' -c copy -f matroska pipe:1 | ffplay -autoexit -i -")
+        os.popen(f"ffmpeg -t {h - (dt1 - dt2)} -i 'rtsp://admin:@192.168.1.108:554/trackID={i}' -y -c copy '{folder}/{dt1.minute}.{dt1.second}.mkv' -c copy -f matroska pipe:1 | ffplay -autoexit -i -")
         time.sleep(0.5)
     dt1 = datetime.datetime.fromtimestamp(time.time())
     dt2 = dt1.replace(minute=0, second=0)
